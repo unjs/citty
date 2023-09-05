15e9822312708af450ac246682cb53ef3f5942de
barbapapazes <e.soubiran25@gmail.com>
2023-09-06 00:06:45 +0200

    feat: support `-v` or `--version`

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,18 @@ export async function runMain<T extends ArgsDef = ArgsDef>(
     if (rawArgs.includes("--help") || rawArgs.includes("-h")) {
       await showUsage(...(await resolveSubCommand(cmd, rawArgs)));
       process.exit(0);
+    } else if (rawArgs.includes("--version") || rawArgs.includes("-v")) {
+      const meta = typeof cmd.meta === "function" ? (await cmd.meta()) : await cmd.meta;
+
+      if (!meta) {
+        throw new CLIError("No meta specified", "E_NO_META");
+      }
+
+      if (!meta?.version) {
+        throw new CLIError("No version specified", "E_NO_VERSION");
+      }
+
+      consola.info(meta.version);
     } else {
       await runCommand(cmd, { rawArgs });
     }
