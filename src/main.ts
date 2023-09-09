4bf355e367f06aacb56e43012222436571a3b43d
Estéban <esteban.soubiran@insa-cvl.fr>
2023-09-09 14:00:52 +0200

    feat: support `--version` (#67)

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,13 @@ export async function runMain<T extends ArgsDef = ArgsDef>(
     if (rawArgs.includes("--help") || rawArgs.includes("-h")) {
       await showUsage(...(await resolveSubCommand(cmd, rawArgs)));
       process.exit(0);
+    } else if (rawArgs.length === 1 && rawArgs[0] === "--version") {
+      const meta =
+        typeof cmd.meta === "function" ? await cmd.meta() : await cmd.meta;
+      if (!meta?.version) {
+        throw new CLIError("No version specified", "E_NO_VERSION");
+      }
+      consola.log(meta.version);
     } else {
       await runCommand(cmd, { rawArgs });
     }
