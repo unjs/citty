416cd8f172ab5961c0fab114294c00080eab532b
Estéban <esteban.soubiran@insa-cvl.fr>
2023-09-09 13:49:01 +0200

    feat: support cleanup hook (#64)

--- playground/cli.ts
+++ playground/cli.ts
@@ -6,6 +6,12 @@ const main = defineCommand({
     version: "1.0.0",
     description: "Citty playground CLI",
   },
+  setup() {
+    console.log("Setup");
+  },
+  cleanup() {
+    console.log("Cleanup");
+  },
   subCommands: {
     build: () => import("./commands/build").then((r) => r.default),
     deploy: () => import("./commands/deploy").then((r) => r.default),
