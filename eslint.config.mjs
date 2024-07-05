1f938a9da76393900d04573656232be3edeeaf49
gangan <44604921+shinGangan@users.noreply.github.com>
2024-07-05 21:44:44 +0900

    fix: remove dist, coverage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import unjs from "eslint-config-unjs";
 
 export default unjs({
-  ignores: ["dist", "node_modules", "src/_parser.ts", "coverage"],
+  ignores: ["node_modules", "src/_parser.ts"],
   rules: {
     // https://github.com/sindresorhus/eslint-plugin-unicorn
     "unicorn/no-process-exit": 0,
